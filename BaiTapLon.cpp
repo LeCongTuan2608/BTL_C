5141d6fbf4580d1fc0c8d4bedaac70e9a2b1a457
LeCongTuan <93369087+LeCongTuan2608@users.noreply.github.com>
2021-11-10 14:42:11 +0700

    Update BaiTapLon.cpp

--- BaiTapLon.cpp
+++ BaiTapLon.cpp
@@ -11,61 +11,6 @@ class SinhVien{
 		virtual void Xuat() = 0;
 };
 
-// ==================class 1 sinh vien======================
-class ThongTin: public SinhVien{
-	private:
-		string HoTen, MSSV, GioiTinh, QueQuan;
-		int Ngay, Thang, Nam;	
-	public:
-		ThongTin();
-		~ThongTin();
-		void Nhap();
-		void Xuat();
-};
-// Ham tao
-ThongTin::ThongTin(){
-	HoTen = "";
-	MSSV = "";
-	GioiTinh = "";
-	QueQuan = "";
-	Ngay = 0;
-	Thang = 0;
-	Nam = 0;	
-}
-// ham huy
-ThongTin::~ThongTin(){
-	
-}
-
-//ham nhap 1 sinh vien
-void ThongTin::Nhap(){
-	cout <<"Ho va ten: ";
-	getline(cin, HoTen);
-	cout <<"Ma so sinh vien: ";
-	getline(cin, MSSV);
-	cout <<"Gioi tinh: ";
-	getline(cin, GioiTinh);
-	cout <<"Que quan: ";
-	getline(cin, QueQuan);
-	cout <<"Ngay sinh: "<<endl;
-	cout <<"Ngay ";
-	cin >> Ngay;
-	cout <<"Thang ";
-	cin >> Thang;
-	cout <<"Nam ";
-	cin >> Nam;
-
-}
-//================================ class thong tin =========================
-//ham xuat 1 sinh vien
-void ThongTin::Xuat(){
-	cout <<"\nHo va ten: "<<HoTen<<endl;
-	cout <<"Ma so sinh vien: "<<MSSV<<endl;
-	cout <<"Gioi tinh: "<< GioiTinh <<endl;
-	cout <<"Que quan: "<< QueQuan <<endl;
-	cout <<"Ngay sinh: "<<Ngay<<"-"<<Thang<<"-"<<Nam<<endl;
-
-}
 //=========================== class diem so =======================
 class DiemSo:public SinhVien{
 	private:
@@ -73,6 +18,15 @@ class DiemSo:public SinhVien{
 	public:
 		DiemSo();
 		~DiemSo();
+		
+		float getDTB();
+		float getDRL();
+		float getDC();
+		
+		void setDTB();
+		void setDRL();
+		void setDC();
+		
 		void Nhap();
 		void Xuat();
 		
@@ -86,6 +40,17 @@ DiemSo::DiemSo(){
 //ham huy
 DiemSo::~DiemSo(){
 	
+}
+//ham tra ve dtb
+float DiemSo::getDTB(){
+	return DTB;
+}
+//ham tra ve drl
+float DiemSo::getDRL(){
+	return DRL;
+}//ham tra ve dc
+float DiemSo::getDC(){
+	return DC;
 }
 
 //ham nhap diem 1 sinh vien
@@ -114,6 +79,13 @@ class NGANH: public SinhVien{
 	public:
 		NGANH();
 		~NGANH();
+		
+		string getNganh();
+		int getKhoa();
+		
+		void setNganh();
+		void setKhoa();
+		
 		void Nhap();
 		void Xuat();	
 };
@@ -139,29 +111,115 @@ void NGANH::Xuat(){
 	cout <<"Khoa: "<<Khoa<<endl;
 }
 
-//=============================class node ==============================
+// ==================class thong tin======================
+class ThongTin: public SinhVien{
+	private:
+		string HoTen, MSSV, GioiTinh, QueQuan;
+		int Ngay, Thang, Nam;	
+		DiemSo diemso;
+		NGANH nganh;
+		
+	public:
+		ThongTin();
+		~ThongTin();
+		
+		DiemSo getdiemso();
+		NGANH getnganh();
+		
+		void Nhap();
+		void Xuat();
+};
+// Ham tao mac dinh
+ThongTin::ThongTin(){
+	HoTen = "";
+	MSSV = "";
+	GioiTinh = "";
+	QueQuan = "";
+	Ngay = 0;
+	Thang = 0;
+	Nam = 0;
+	
+}
+// ham huy
+ThongTin::~ThongTin(){
+	
+}
+
+// return ve class diem so
+DiemSo ThongTin::getdiemso(){
+	return diemso;
+}
+
+
+// return ve class nganh
+NGANH ThongTin::getnganh(){
+	return nganh;
+}
+
+
+//ham nhap thong tin 1 sinh vien
+void ThongTin::Nhap(){
+	cout <<"Ho va ten: ";
+	cin.ignore();
+	getline(cin, HoTen);
+	cout <<"Ma so sinh vien: ";
+	getline(cin, MSSV);
+	cout <<"Gioi tinh: ";
+	getline(cin, GioiTinh);
+	cout <<"Que quan: ";
+	getline(cin, QueQuan);
+	cout <<"Ngay sinh: "<<endl;
+	cout <<"Ngay ";
+	cin >> Ngay;
+	cout <<"Thang ";
+	cin >> Thang;
+	cout <<"Nam ";
+	cin >> Nam;
+	diemso.Nhap();
+	nganh.Nhap();
+
+}
+//ham xuat thong tin 1 sinh vien
+void ThongTin::Xuat(){
+	cout <<"\nHo va ten: "<<HoTen<<endl;
+	cout <<"Ma so sinh vien: "<<MSSV<<endl;
+	cout <<"Gioi tinh: "<< GioiTinh <<endl;
+	cout <<"Que quan: "<< QueQuan <<endl;
+	cout <<"Ngay sinh: "<<Ngay<<"-"<<Thang<<"-"<<Nam<<endl;
+	diemso.Xuat();
+	nganh.Xuat();
+}
+
+
+
+//============================= class node ==============================
 class Node{
 	public:
-		int data;
-		Node *next;	
+		ThongTin data;
+		Node *next;
 };
 
-// ====================class danh sach =================================
+
+// ==================== class danh sach =================================
 class SList{
-	private:
+	public:
 		Node *Head;
 		Node *Tail;
 		int size;
 	public:
 		SList();
 		~SList();
-		Node *CreateNode(int data);
-		void AddSV(int data);
-		void DeleteSV(int data);
-		void SearchName(int data);
-		void SearchID(int data);
+		
+		Node *CreateNode(ThongTin sv);
+		void AddFirst(ThongTin sv);
+		void AddLast(ThongTin sv);
+		void DeleteSV(ThongTin sv);
+		void SearchName(ThongTin sv);
+		void SearchID(ThongTin sv);
 		void SapXepDTB();
-		void XuatDS(int data);			
+		void NhapDS(ThongTin &sv);
+		void XuatDS();	
+				
 };
 
 //ham tao mac dinh
@@ -170,29 +228,53 @@ SList::SList(){
 	Tail = NULL;
 	size = 0;
 }
-
+SList::~SList(){
+}
 //ham tao ra 1 node
-Node* SList::CreateNode(int data){
-
+Node* SList::CreateNode(ThongTin sv){
+	Node* p =new Node;
+	p->data = sv;
+	p->next = NULL;
+	return p;
 }
 
-//ham them 1 sinh vien
-void SList::AddSV(int data){
+//ham them 1 sinh vien vao dau danh sach
+void SList::AddFirst(ThongTin sv){
+	Node *p = CreateNode(sv);
+	p->next = Head;
+	if(size == 0)
+		Tail = p;
+	size++;
+}
+
+//ham them 1 sinh vien vao cuoi danh sach
+void SList::AddLast(ThongTin sv){
+	Node*p = CreateNode(sv);
 	
+	if(size == 0){
+		Tail = Head = p;
+		Tail->next = NULL;
+		Head->next = NULL;
+	}
+	else{
+		Tail->next = p;
+		Tail =p;
+	}
+	size++;
 }
 
 //ham xoa 1 sinh vien
-void SList::DeleteSV(int data){
+void SList::DeleteSV(ThongTin sv){
 	
 }
 
 //ham tim kiem sinh vien theo ten
-void SList::SearchName(int data){
+void SList::SearchName(ThongTin sv){
 	
 }
 
 //ham tim kiem sinh vien theo mssv
-void SList::SearchID(int data){
+void SList::SearchID(ThongTin sv){
 	
 }
 
@@ -201,27 +283,51 @@ void SList::SapXepDTB(){
 	
 }
 
+void SList::NhapDS(ThongTin &sv){
+	sv.Nhap();
+}
+
 //ham xuat ra danh sach sinh vien
-void SList::XuatDS(int data){
-	
+void SList::XuatDS(){
+	for(Node *t = Head; t != NULL ; t = t->next){
+		t->data.Xuat();
+	}
 }
 
 
 //========================== ham main ============================
 
 int main(){
-	ThongTin tt;
-	DiemSo diem;
-	NGANH nganh;
+	SList list ;
+	int n;
+	cout <<"So luong sinh vien can nhap: ";
+	cin >> n;
 	
-	SinhVien *sv= &tt;
-	sv->Nhap();
-	diem.Nhap();
-	nganh.Nhap();
-
-	sv->Xuat();
-	diem.Xuat();
-	nganh.Xuat();
+	//bo vao` vong` lap while ,, thay i<n = list.szie
+	for(int i=0; i<n;i++){
+		ThongTin sv;
+		cout <<"Sinh vien thu "<<i+1<<endl<<endl;
+		sv.Nhap();
+		list.AddLast(sv);
+	}
+	
+		list.XuatDS();
+	
+	
+	
+	
+//	ThongTin tt;
+//	DiemSo diem;
+//	NGANH nganh;
+//	
+//	SinhVien *sv= &tt;
+//	sv->Nhap();
+//	diem.Nhap();
+//	nganh.Nhap();
+//
+//	sv->Xuat();
+//	diem.Xuat();
+//	nganh.Xuat();
 
 	return 0;
 }
